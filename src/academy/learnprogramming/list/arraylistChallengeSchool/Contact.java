ed7d1293b309f29b236495d18048c0e557c5a532
Bogdan <tatarubog@gmail>
2022-09-22 11:30:34 +0200

    Mobile application - school

--- src/academy/learnprogramming/list/arraylistChallengeSchool/Contact.java
+++ src/academy/learnprogramming/list/arraylistChallengeSchool/Contact.java
@@ -1,4 +1,25 @@
 package academy.learnprogramming.list.arraylistChallengeSchool;
 
 public class Contact {
+
+    private String name;
+    private String phoneNUmber;
+
+    public Contact(String name, String phoneNUmber) {
+        this.name = name;
+        this.phoneNUmber = phoneNUmber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNUmber() {
+        return phoneNUmber;
+    }
+
+    // we don't have to create a new object instance for this contact class.
+    public static Contact createContact(String name, String phoneNUmber){
+        return new Contact(name, phoneNUmber);
+    }
 }
